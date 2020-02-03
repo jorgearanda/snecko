ee1c3172dfc7ab4083885614b2e0f4617b3c8ec3
Jorge Aranda <jorge.aranda@cuevano.ca>
2020-02-02 21:44:36 -0800

    Add boilerplate structure from starter snake

---
+++ app/main.py
@@ -0,0 +1,69 @@
+import json
+import os
+import random
+import bottle
+
+from api import ping_response, start_response, move_response, end_response
+
+
+@bottle.route("/")
+def index():
+    return """
+    Snecko!
+    """
+
+
+@bottle.route("/static/<path:path>")
+def static(path):
+    """
+    Given a path, return the static file located relative
+    to the static folder.
+    """
+    return bottle.static_file(path, root="static/")
+
+
+@bottle.post("/ping")
+def ping():
+    """
+    A keep-alive endpoint.
+    """
+    return ping_response()
+
+
+@bottle.post("/start")
+def start():
+    data = bottle.request.json
+    print(json.dumps(data))
+    color = "#00FF00"
+
+    return start_response(color)
+
+
+@bottle.post("/move")
+def move():
+    data = bottle.request.json
+    print(json.dumps(data))
+    directions = ["up", "down", "left", "right"]
+    direction = random.choice(directions)
+
+    return move_response(direction)
+
+
+@bottle.post("/end")
+def end():
+    data = bottle.request.json
+    print(json.dumps(data))
+
+    return end_response()
+
+
+# Expose WSGI app (so gunicorn can find it)
+application = bottle.default_app()
+
+if __name__ == "__main__":
+    bottle.run(
+        application,
+        host=os.getenv("IP", "0.0.0.0"),
+        port=os.getenv("PORT", "8080"),
+        debug=os.getenv("DEBUG", True),
+    )
